c344688323e44d83696a67b4d29189689de7cde3
neo-kjb <anaszawahreh.dev@gmail.com>
2022-10-27 23:29:34 +0300

    we can see the sagments now

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const{Engine , Render, Runner , World , Bodies} = Matter
 const engine=Engine.create();
 const{world}=engine;
-const cells = 5;
+const cells = 10;
 const height=600;
 const width=600;
+const unitLength = width/cells;
 const render=Render.create({
     element: document.body,
     engine:engine,
@@ -28,3 +29,102 @@ World.add(world,walls);
 const grid= Array(cells).fill(null).map(()=>Array(cells).fill(false));
 const horizontal =Array(cells-1).fill(null).map(()=>Array(cells).fill(false));
 const vertical = Array(cells).fill(null).map(()=>Array(cells-1).fill(false));
+const startRow = Math.floor(Math.random()*cells);
+const startColumn = Math.floor(Math.random()*cells);
+const shuffle=(arr)=>{
+    let counter = arr.length;
+    while(counter > 0){
+        const index = Math.floor(Math.random()*counter);
+        counter--;
+        const temp = arr[counter];
+        arr[counter]=arr[index];
+        arr[index]=temp;
+    }
+    return arr;
+};
+
+
+
+const stepThroughCell=(row,column)=>{
+    if(grid[row][column]){
+        return;
+    }
+    
+
+    grid[row][column]=true;
+
+
+    const neighbors= shuffle([
+        [row,column-1,'left'],
+        [row-1,column,'up'],
+        [row,column+1,'right'],
+        [row+1,column,'down']
+    ])
+
+
+    for (let neighbor of neighbors){
+        const[nextRow,nextColumn,direction]=neighbor;
+
+        if (nextRow<0 || nextRow >= cells || nextColumn <0 || nextColumn >=cells){
+            continue;
+        }
+        if (grid[nextRow][nextColumn]){
+            continue;
+        };
+        if (direction === 'left') {
+            vertical[row][column - 1] = true;
+          } else if (direction === 'right') {
+            vertical[row][column] = true;
+          } else if (direction === 'up') {
+            horizontal[row - 1][column] = true;
+          } else if (direction === 'down') {
+            horizontal[row][column] = true;
+          }
+        stepThroughCell(nextRow,nextColumn);
+    }
+
+
+
+
+};
+stepThroughCell(startRow,startColumn)
+
+
+vertical.forEach((row,rowIdx)=>{
+    row.forEach((open,columnIdx)=>{
+        if(open){
+            return;
+        }
+        const wall = Bodies.rectangle(
+            (columnIdx*unitLength)+unitLength,
+            (rowIdx*unitLength)+(unitLength/2),
+            5,
+            unitLength,
+            {
+                isStatic: true
+            }
+        )
+        World.add(world,wall)
+        })
+});
+
+
+horizontal.forEach((row,rowIdx)=>{
+    row.forEach((open,columnIdx)=>{
+        if (open){
+            return
+        }
+        const wall = Bodies.rectangle(
+            columnIdx*unitLength+unitLength/2,
+            rowIdx*unitLength+unitLength,
+            unitLength,
+            5,
+            {
+                isStatic:true
+            }
+        )
+        World.add(world,wall)
+
+    })
+})
+
